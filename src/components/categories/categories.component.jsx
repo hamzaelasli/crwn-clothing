7eaaafd3aef32bbd5747d4276fd772ac3d598c1b
hamzaelasli <elaslihamza497@gmail.com>
2023-02-05 15:37:43 +0100

    First commit adding categories component and category item component

---
+++ src/components/categories/categories.component.jsx
@@ -0,0 +1,12 @@
+import CategoryItem from "../category-item/category.component";
+import "./categories.styles.scss";
+const Categories = ({ categories }) => {
+  return (
+    <div className="categories--container">
+      {categories.map((category) => {
+        return <CategoryItem key={category.id} category={category} />;
+      })}
+    </div>
+  );
+};
+export default Categories;
---
+++ src/components/category-item/category.component.jsx
@@ -0,0 +1,18 @@
+import "./category.styles.scss";
+const CategoryItem = ({ category }) => {
+  const { title, imageUrl } = category;
+  return (
+    <div className="category--container large">
+      <div
+        className="background-image"
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      ></div>
+      <div className="category--body-container">
+        <h2>{title}</h2>
+        <p>shop now</p>
+      </div>
+    </div>
+  );
+};
+
+export default CategoryItem;
